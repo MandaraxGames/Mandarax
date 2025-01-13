12ee5c648a3008cacd19d596cd1a2392ab15d684
MandaraxGames <mandaraxgames@gmail.com>
2025-01-13 17:23:01 -0500

    Update mx_general_public.h

--- src/mx_general_public.h
+++ src/mx_general_public.h
@@ -4,8 +4,8 @@
 #include <SDL.h>
 #include <SDL_stdinc.h>
 #include <SDL_timer.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
+//#include <SDL_ttf.h>
+//#include <SDL_image.h>
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 576
