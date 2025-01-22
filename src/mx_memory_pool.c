24a33a8d8f889301c0f4229268c65725975cb123
MandaraxGames <mandaraxgames@gmail.com>
2025-01-21 22:49:04 -0500

    Update mx_memory_pool.c

--- src/mx_memory_pool.c
+++ src/mx_memory_pool.c
@@ -8,19 +8,20 @@ void* create_pool(void) {
   Uint64 i;
   pool->free_blocks = NUM_BLOCKS;
   for (i = 0; i < NUM_BLOCKS; i++) {
-    pool->blocks[i].is_free = TRUE;
+    pool->blocks[i].is_free = SDL_TRUE;
     pool->blocks[i].size = 0;
   }
   return (void*)pool;
 }
 
-void* pool_alloc(void* pool, size_t size) {
+void* pool_alloc(void* pool_ptr, size_t size) {
+  MX_PoolManager* pool = (MX_PoolManager*)pool_ptr;
   if (!pool || size > BLOCK_SIZE) return NULL;
   
   Uint64 i;
   for (i = 0; i < NUM_BLOCKS; i++) {
     if (pool->blocks[i].is_free) {
-      pool->blocks[i].is_free = FALSE;
+      pool->blocks[i].is_free = SDL_FALSE;
       pool->blocks[i].size = size;
       pool->free_blocks--;
       return (void*)pool->blocks[i].data;
@@ -29,13 +30,15 @@ void* pool_alloc(void* pool, size_t size) {
   return NULL;
 }
 
-void pool_free(void* pool, void* ptr) {
+void pool_free(void* pool_ptr, void* p_ptr) {
+  MX_PoolManager* pool = (MX_PoolManager*)pool_ptr;
+  MX_Pool* ptr = (MX_Pool*)p_ptr;
   if (!pool || !ptr) return;
   
   Uint64 i;
   for (i = 0; i < NUM_BLOCKS; i++) {
     if (pool->blocks[i].data == ptr) {
-      pool->blocks[i].is_free = TRUE;
+      pool->blocks[i].is_free = SDL_TRUE;
       pool->blocks[i].size = 0;
       pool->free_blocks++;
       return;
